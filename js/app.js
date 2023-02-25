1ce9a9ce23798886023f81ee50ac9f19958a40cd
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-02-25 22:40:15 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -18,9 +18,9 @@ const displayPhones = phones =>{
         div.classList.add('col');
         div.innerHTML = `
         <div class="card">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="${individualPhones.image}" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
+                    <h5 class="card-title">${individualPhones.phone_name}</h5>
                     <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
             </div>
         </div>
