c8ed62838aab6ac98c75def040af087b3c6cfd1b
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-02-25 22:38:09 +0600

    update

--- js/app.js
+++ js/app.js
@@ -9,8 +9,24 @@ const loadPhone = async() =>{
 // Display Phones Data;
 const displayPhones = phones =>{
     console.log(phones)
+    // Div Container;
+    const divContainer = document.getElementById('div-container');
     phones.forEach(individualPhones=>{
         console.log(individualPhones)
+        
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card">
+            <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+        `;
+        divContainer.appendChild(div);
+
     })
 }
 // Calling phone Data Function;
